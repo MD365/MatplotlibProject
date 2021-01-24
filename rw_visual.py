d5c8fd7040a3ea0396215da15d56f65d7d3eb8b3
马达 <mada365@gmail.com>
2021-01-24 21:31:54 +0800

    练习完成

--- rw_visual.py
+++ rw_visual.py
@@ -4,9 +4,19 @@ from random_walk import RandomWalk
 
 #创建个randowwalk实例，并将点都绘制出来
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s= 15)
+    #设置绘图窗口的尺寸
+    plt.figure(figsize=(10,6))
+    point_numbers = list(range(rw.num_points))
+    # plt.scatter(rw.x_values, rw.y_values, s= 1, c= point_numbers,cmap=plt.cm.Blues,edgecolors='none')
+    plt.plot(rw.x_values,rw.y_values)
+    # 突出起点和终点
+    # plt.scatter(0,0,c='green',edgecolors='none', s=10)
+    # plt.scatter(rw.x_values[-1], rw.y_values[-1],c='red',edgecolors='none', s=10)
+    #隐藏坐标轴
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
     plt.show()
 
     keep_running = input("make another walk(y/n):")
